4bbded6943c9b7bcffee4fa3e752020bbf984a69
精神薇 <zhengwei931102@163.com>
2018-10-18 16:37:40 +0800

    GIFImage

---
+++ NEUImageView.h
@@ -0,0 +1,15 @@
+//
+//  NEUImageView.h
+//  NEUGIFImage
+//
+//  Created by Yong Li on 14-3-2.
+//  Copyright (c) 2014年 Yong Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NEUImageView : UIImageView
+
+@property (nonatomic, copy) NSString *runLoopMode;
+
+@end
